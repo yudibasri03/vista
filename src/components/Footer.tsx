f15a1cecfe0c310fcc8723b17d4c1508e4297c47
yudibasri03 <125482932+yudibasri03@users.noreply.github.com>
2025-12-04 15:52:14 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,13 @@ export default function Footer() {
         <div className="grid md:grid-cols-4 gap-8 mb-8">
           <div className="md:col-span-2">
             <div className="flex items-center gap-2 mb-4">
-                <div className="w-60 h-20 bg-amber-500 rounded-lg flex items-center justify-center">
-                <TrendingUp className="w-6 h-6 text-slate-900" />
-                </div>
+              <div className="flex items-center mb-4">
+              <img 
+                src="/vista-logo.png"
+                alt="Vista Logo"
+                className="h-10 w-auto md:h-12"
+              />
+              </div>
             </div>
             <p className="text-gray-400 leading-relaxed mb-4 max-w-md">
               Ekosistem trading terintegrasi yang membantu Anda belajar, berlatih, dan berkembang dengan pendekatan berbasis data dan risk management.
